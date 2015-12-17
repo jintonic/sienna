36a5e428b1bfd696f5191c20e8053c2106be60c7
Jing LIU <jing.liu@usd.edu>
2015-12-17 07:14:34 -0600

    output as png

--- pie.R
+++ pie.R
@@ -3,5 +3,5 @@ lbl <- c("Oil", "Fuel", "Insurance", "Exterior", "Car")
 pct <- round(dat/sum(dat)*100)
 lbl <- paste(lbl, pct) # add percents to labels
 lbl <- paste(lbl,"%",sep="") # ad % to labels
-pdf("pie.pdf")
+png("pie.png")
 pie(dat, labels=lbl, main=sprintf("Total: $%.0f",sum(dat)))
