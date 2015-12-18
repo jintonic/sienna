c5f4dd7853c4a7d7c5c841099cf648d95dc2564f
Jing LIU <jing.liu@usd.edu>
2015-12-17 23:27:41 -0600

    renamed

--- pie.R
+++
@@ -1,7 +0,0 @@
-dat <- scan("pie.dat")
-lbl <- c("Oil", "Fuel", "Insurance", "Exterior", "Car")
-pct <- round(dat/sum(dat)*100)
-lbl <- paste(lbl, pct) # add percents to labels
-lbl <- paste(lbl,"%",sep="") # ad % to labels
-png("pie.png")
-pie(dat, labels=lbl, main=sprintf("Total: $%.0f",sum(dat)))
